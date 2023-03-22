2171e2053a2ca51cb9088d7386ddbba40e023640
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-03-22 10:45:06 +0200

    Update MyTaskListener with updated rule

--- Codes/MyTaskListener.java
+++ Codes/MyTaskListener.java
@@ -1,25 +1,32 @@
-public class MyTaskListener extends Task1BaseListener {
+public class MyTaskListener extends Task1BaseListener{
     static int blockCounter = 0;
     static String result = "";
 
     @Override
-    public void enterOpenCurlybraces(Task1Parser.OpenCurlybracesContext ctx) {
-        super.enterOpenCurlybraces(ctx);
-        System.out.print(ctx.getText()+"\n//Block "+blockCounter++ +"\n");
-        result += ctx.getText()+"\n//Block "+blockCounter +"\n";
-    }
-
-    @Override
-    public void enterStatementState(Task1Parser.StatementStateContext ctx) {
-        super.enterStatementState(ctx);
+    public void enterLineStartAndEnd(Task1Parser.LineStartAndEndContext ctx) {
+        super.enterLineStartAndEnd(ctx);
         System.out.print(ctx.getText());
         result += ctx.getText();
     }
 
     @Override
-    public void exitEndCurlybraces(Task1Parser.EndCurlybracesContext ctx) {
-        super.exitEndCurlybraces(ctx);
+    public void enterOpenCurlybraces(Task1Parser.OpenCurlybracesContext ctx) {
+        super.enterOpenCurlybraces(ctx);
+        System.out.println("{\n// Block no."+blockCounter++);
+        result += "{ Block no."+blockCounter;
+    }
+
+    @Override
+    public void enterStatement_state(Task1Parser.Statement_stateContext ctx) {
+        super.enterStatement_state(ctx);
         System.out.print(ctx.getText());
+        result += ctx.getText();
+    }
+
+    @Override
+    public void enterEndCurlybraces(Task1Parser.EndCurlybracesContext ctx) {
+        super.enterEndCurlybraces(ctx);
+        System.out.println(ctx.getText());
         result += ctx.getText();
     }
 }
