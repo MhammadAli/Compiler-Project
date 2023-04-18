0830c4ab4b57e47d6fc3651618711df82c67ded6
Muhammad Ali <66525657+MhammadAli@users.noreply.github.com>
2023-04-18 16:58:31 +0200

    update Main

--- Codes/Main.java
+++ Codes/Main.java
@@ -5,22 +5,23 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int[] Array;
-        String inputFile = "input.java";
-        FileInputStream inputStream = new FileInputStream(inputFile);
-        ANTLRInputStream input = new ANTLRInputStream(inputStream);
-        JavaLexer lexer = new JavaLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        JavaParser parser = new JavaParser(tokens);
-        ParseTree tree = parser.compilationUnit();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
-        walker.walk(new MyTaskListener(rewriter), tree);
+        for (int i = 1; i <=5 ; i++){
+            String inputFile = "inputs/input"+i+".java";
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            ANTLRInputStream input = new ANTLRInputStream(inputStream);
+            JavaLexer lexer = new JavaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            JavaParser parser = new JavaParser(tokens);
+            ParseTree tree = parser.compilationUnit();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
+            walker.walk(new MyTaskListener(rewriter), tree);
 
-        File output = new File("output.java");
-        output.createNewFile();
-        FileWriter w = new FileWriter("output.java");
-        w.write(rewriter.getText());
-        w.close();
+            File output = new File("src/outputs/output"+i+".java");
+            output.createNewFile();
+            FileWriter w = new FileWriter("src/outputs/output"+i+".java");
+            w.write(rewriter.getText());
+            w.close();
+        }
     }
 }
