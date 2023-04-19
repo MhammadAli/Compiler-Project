05269abb22d8397f5243ff0ba840ab3c920bb31a
Muhammad Ali <66525657+MhammadAli@users.noreply.github.com>
2023-04-19 16:47:21 +0200

    Update HTMLGen to perform pipeline

--- Codes/HTMLGen.java
+++ Codes/HTMLGen.java
@@ -9,24 +9,31 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 
 public class HTMLGen {
-    public static void main(String[] args) throws Exception {
+    public static void main() throws Exception {
+        for (int i = 1; i <= 5; i++) {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec("java src/outputs/output" + i + ".java");
+            try {
+                pr.waitFor();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            String inputFile = "src/outputs/output" + i + ".java";
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            ANTLRInputStream input = new ANTLRInputStream(inputStream);
+            JavaLexer lexer = new JavaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            JavaParser parser = new JavaParser(tokens);
+            ParseTree tree = parser.compilationUnit();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
+            walker.walk(new HTMLListener(rewriter, "output" + i + ".txt"), tree);
 
-           String inputFile = "src/outputs/output5.java";
-           FileInputStream inputStream = new FileInputStream(inputFile);
-           ANTLRInputStream input = new ANTLRInputStream(inputStream);
-           JavaLexer lexer = new JavaLexer(input);
-           CommonTokenStream tokens = new CommonTokenStream(lexer);
-           JavaParser parser = new JavaParser(tokens);
-           ParseTree tree = parser.compilationUnit();
-           ParseTreeWalker walker = new ParseTreeWalker();
-           TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
-           walker.walk(new HTMLListener(rewriter,"output.txt"), tree);
-
-           File output = new File("html/output.html");
-           output.createNewFile();
-           FileWriter w = new FileWriter("html/output.html");
-           w.write(rewriter.getText());
-           w.close();
-
+            File output = new File("html/output" + i + ".html");
+            output.createNewFile();
+            FileWriter w = new FileWriter("html/output" + i + ".html");
+            w.write(rewriter.getText());
+            w.close();
+        }
     }
 }
